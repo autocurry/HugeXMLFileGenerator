5676426dfcba4f0cb8af032982f0505292719d2a
autocurry <sandeepsekhar555@gmail.com>
2020-05-07 11:34:55 +0530

    updated the file generator with 15 rows data

--- HugeFileGenerator.py
+++ HugeFileGenerator.py
@@ -8,7 +8,21 @@ tpl_xml = '''
 % for i in data:
 <employee>
 <id>${i}</id>
-<name>name</name>
+<name1>name1</name1>
+<name2>name2</name2>
+<name3>name3</name3>
+<name4>name4</name4>
+<name5>name5</name5>
+<name6>name6</name6>
+<name7>name7</name7>
+<name8>name8</name8>
+<name9>name9</name9>
+<name10>name10</name10>
+<name11>name11</name11>
+<name12>name12</name12>
+<name13>name13</name13>
+<name14>name14</name14>
+<name15>name15</name15>
 </employee>
 % endfor
 </employeedata>
@@ -18,6 +32,6 @@ tpl_xml = '''
 
 tpl = Template(tpl_xml)
 
-with open('outputlarge.xml', 'w') as f:
-    ctx = Context(f, data=range(10000000))
+with open('output300.xml', 'w') as f:
+    ctx = Context(f, data=range(300))
     tpl.render_context(ctx)
